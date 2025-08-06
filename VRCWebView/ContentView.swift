c354bc0112d42de1ca84d2b54b4d06fa72e7fa8b
AariyJP <aariyjp@aariy.net>
2025-08-06 15:02:05 +0900

    Initial commit

---
+++ VRCWebView/ContentView.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+struct ContentView: View {
+    let websiteURL = URL(string: "https://vrchat.com/home")!
+    @State private var isLoading = false
+    @State private var webViewReloadTrigger = false
+    let appColor = Color(
+        red: 7/255.0,
+        green: 36/255.0,
+        blue: 43/255.0
+    )
+    
+    
+    var body: some View {
+//        NavigationView {
+            
+            ZStack {
+                
+                ScrollView {
+                    let screenHeight = UIScreen.main.bounds.height
+                    let statusBarHeight = UIApplication.shared.statusBarFrame.height
+                    let appHeight = screenHeight - statusBarHeight
+                    GeometryReader { geometry in
+                        WebView(url: websiteURL, isLoading: $isLoading, reloadTrigger: webViewReloadTrigger)
+                            .background(appColor)
+                    }
+                    .frame(minHeight: appHeight)
+                    .background(appColor)
+                    
+                }
+                .ignoresSafeArea(edges: .bottom)
+                .background(appColor)
+                .refreshable {
+                    webViewReloadTrigger.toggle()
+                }
+                if isLoading {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .gray))
+                        .scaleEffect(2.0)
+                }
+                    
+            }
+//        }
+    }
+}
+
+// Preview
+#Preview {
+    ContentView()
+}
---
+++ VRCWebView/VRCWebView.swift
@@ -0,0 +1,86 @@
+import SwiftUI
+import WebKit
+import SafariServices
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    @Binding var isLoading: Bool
+    let reloadTrigger: Bool
+    
+    class Coordinator: NSObject, WKNavigationDelegate, WKUIDelegate {
+        var parent: WebView
+        var lastReloadTrigger: Bool = false
+        var viewController: UIViewController?
+        
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+            parent.isLoading = true
+        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            parent.isLoading = false
+        }
+        
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            parent.isLoading = false
+        }
+        
+        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            parent.isLoading = false
+        }
+        
+        func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+            if navigationAction.targetFrame == nil {
+                if let url = navigationAction.request.url {
+                    let safariViewController = SFSafariViewController(url: url)
+//                    safariViewController.modalPresentationStyle = .pageSheet
+                    self.viewController?.present(safariViewController, animated: true)
+                }
+            }
+            return nil
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let configuration = WKWebViewConfiguration()
+        // 拡大縮小を無効にするJavaScript
+        let scriptSource = """
+        var meta = document.createElement('meta');
+        meta.name = 'viewport';
+        meta.content = 'width=device-width, initial-scale=0.55, user-scalable=no';
+        document.head.appendChild(meta);
+        """
+        // WKUserScriptを作成し、configurationに追加
+        let userScript = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        configuration.userContentController.addUserScript(userScript)
+        
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.navigationDelegate = context.coordinator
+        webView.uiDelegate = context.coordinator // Set the UI delegate
+        webView.allowsBackForwardNavigationGestures = true
+        webView.allowsLinkPreview = true
+        webView.isInspectable = true
+        
+        DispatchQueue.main.async {
+            context.coordinator.viewController = webView.window?.rootViewController
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if reloadTrigger != context.coordinator.lastReloadTrigger {
+            uiView.reload()
+        }
+        context.coordinator.lastReloadTrigger = reloadTrigger
+    }
+}
